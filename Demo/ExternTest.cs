f9781608f6276be6a08f01d82a228f344f3002c9
pwang86 <pwang86@gmail.com>
2019-09-28 22:16:45 +1000

    add testoverride

--- Demo/ExternTest.cs
+++ Demo/ExternTest.cs
@@ -12,12 +12,12 @@ namespace Demo
 		[DllImport("User32.dll", CharSet = CharSet.Unicode)]
 		public static extern int MessageBox(IntPtr h, string m, string c, int type);
 
-		static int Main()
-		{
-			string myString;
-			Console.Write("Enter your message: ");
-			myString = Console.ReadLine();
-			return MessageBox((IntPtr)0, myString, "My Message Box", 0);
-		}
+		//static int Main()
+		//{
+		//	string myString;
+		//	Console.Write("Enter your message: ");
+		//	myString = Console.ReadLine();
+		//	return MessageBox((IntPtr)0, myString, "My Message Box", 0);
+		//}
 	}
 }
---
+++ Demo/TestOverride.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+	class TestOverride
+	{
+		public class Employee
+		{
+			public string name;
+
+			// it may be accessed only by this class and derived classes.
+			protected decimal basepay;
+
+			public Employee(string name, decimal basepay)
+			{
+				this.name = name;
+				this.basepay = basepay;
+			}
+
+			public virtual decimal CalculatePay()
+			{
+				return basepay;
+			}
+		}
+
+		public class SalesEmployee : Employee
+		{
+			private decimal salesbonus;
+
+			public SalesEmployee(string name, decimal basepay, decimal salesbonus) : base(name, basepay)
+			{
+				this.salesbonus = salesbonus;
+			}
+
+			public override decimal CalculatePay()
+			{
+				return basepay + salesbonus;
+			}
+		}
+
+		static void Main()
+		{
+			var employee1 = new SalesEmployee("Rob", 1000, 500);
+			var employee2 = new Employee("Bob", 1200);
+
+			Console.WriteLine("Employee " + employee1.name + " earned: " + employee1.CalculatePay());
+
+			Console.WriteLine("Employee " + employee2.name + " earned: " + employee2.CalculatePay());
+		}
+	}
+}
