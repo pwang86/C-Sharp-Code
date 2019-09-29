46dc9cff697c512854214c2684a331c307a97e15
pwang86 <pwang86@gmail.com>
2019-09-29 12:19:46 +1000

    add static demo

---
+++ Demo/StaticDemo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+	class StaticDemo
+	{
+		public class Employee
+		{
+			public static int counter;
+			public static int AddEmployee()
+			{
+				return ++counter;
+			}
+		}
+
+		class MainClass : Employee
+		{
+			static void Main()
+			{
+				string n = Console.ReadLine();
+				Employee.counter = Int32.Parse(n);
+				Employee.AddEmployee();
+
+				Console.WriteLine($"new number of employees: {Employee.counter}");
+			}
+		}
+	}
+}
--- Demo/TestOverride.cs
+++ Demo/TestOverride.cs
@@ -8,48 +8,48 @@ namespace Demo
 {
 	class TestOverride
 	{
-		public class Employee
-		{
-			public string name;
+	//	public class Employee
+	//	{
+	//		public string name;
 
-			// it may be accessed only by this class and derived classes.
-			protected decimal basepay;
+	//		// it may be accessed only by this class and derived classes.
+	//		protected decimal basepay;
 
-			public Employee(string name, decimal basepay)
-			{
-				this.name = name;
-				this.basepay = basepay;
-			}
+	//		public Employee(string name, decimal basepay)
+	//		{
+	//			this.name = name;
+	//			this.basepay = basepay;
+	//		}
 
-			public virtual decimal CalculatePay()
-			{
-				return basepay;
-			}
-		}
+	//		public virtual decimal CalculatePay()
+	//		{
+	//			return basepay;
+	//		}
+	//	}
 
-		public class SalesEmployee : Employee
-		{
-			private decimal salesbonus;
+	//	public class SalesEmployee : Employee
+	//	{
+	//		private decimal salesbonus;
 
-			public SalesEmployee(string name, decimal basepay, decimal salesbonus) : base(name, basepay)
-			{
-				this.salesbonus = salesbonus;
-			}
+	//		public SalesEmployee(string name, decimal basepay, decimal salesbonus) : base(name, basepay)
+	//		{
+	//			this.salesbonus = salesbonus;
+	//		}
 
-			public override decimal CalculatePay()
-			{
-				return basepay + salesbonus;
-			}
-		}
+	//		public override decimal CalculatePay()
+	//		{
+	//			return basepay + salesbonus;
+	//		}
+	//	}
 
-		static void Main()
-		{
-			var employee1 = new SalesEmployee("Rob", 1000, 500);
-			var employee2 = new Employee("Bob", 1200);
+	//	static void Main()
+	//	{
+	//		var employee1 = new SalesEmployee("Rob", 1000, 500);
+	//		var employee2 = new Employee("Bob", 1200);
 
-			Console.WriteLine("Employee " + employee1.name + " earned: " + employee1.CalculatePay());
+	//		Console.WriteLine("Employee " + employee1.name + " earned: " + employee1.CalculatePay());
 
-			Console.WriteLine("Employee " + employee2.name + " earned: " + employee2.CalculatePay());
-		}
+	//		Console.WriteLine("Employee " + employee2.name + " earned: " + employee2.CalculatePay());
+	//	}
 	}
 }
