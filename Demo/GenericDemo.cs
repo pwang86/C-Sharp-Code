29f290905497bbeefc8e8cf3eb79b14b109b7e1f
pwang86 <pwang86@gmail.com>
2019-09-30 22:37:09 +1000

    add generic demo

---
+++ Demo/GenericDemo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+	public class GenericDemo<T>
+	{
+		private class Node
+		{
+			public Node(T t)
+			{
+				next = null;
+				data = t;
+			}
+
+			private Node next;
+			public Node Next
+			{
+				get { return next; }
+				set { next = value; }
+			}
+
+			private T data;
+			public T Data
+			{
+				get { return data; }
+				set { data = value; }
+			}
+		}
+		private Node head;
+
+		public GenericDemo()
+		{
+			head = null;
+		}
+
+		public void AddHead(T t)
+		{
+			Node n = new Node(t);
+			n.Next = head;
+			head = n;
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			Node current = head;
+			while (current != null)
+			{
+				yield return current.Data;
+				current = current.Next;
+			}
+		}
+	}
+
+
+}
---
+++ Demo/TestGenericDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+	class TestGenericDemo
+	{
+		static void Main()
+		{
+			GenericDemo<int> list = new GenericDemo<int>();
+
+			for (int x = 0; x < 10; x++)
+			{
+				list.AddHead(x);
+			}
+
+			foreach (int i in list)
+			{
+				System.Console.Write(i + " ");
+			}
+			System.Console.WriteLine("\nDone");
+		}
+	}
+}
