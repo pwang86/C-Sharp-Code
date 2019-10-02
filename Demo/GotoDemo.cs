00b7f3a5ffeed6b12690285a7a80c2ca87d08d60
pwang86 <pwang86@gmail.com>
2019-10-02 17:20:46 +1000

    add exception demo

--- Demo/GotoDemo.cs
+++ Demo/GotoDemo.cs
@@ -8,46 +8,46 @@ namespace Demo
 {
 	class GotoDemo
 	{
-		static void Main()
-		{
-			int x = 200, y = 4;
-			int count = 0;
-			string[,] array = new string[x, y];
+		//static void Main()
+		//{
+		//	int x = 200, y = 4;
+		//	int count = 0;
+		//	string[,] array = new string[x, y];
 
-			// Initialize the array.
-			for (int i = 0; i < x; i++)
-			{
-				for (int j = 0; j < y; j++)
-				{
-					array[i, j] = (++count).ToString();
-				}
-			}
-			Console.Write("Enter the number: ");
-			string myNumber = Console.ReadLine();
+		//	// Initialize the array.
+		//	for (int i = 0; i < x; i++)
+		//	{
+		//		for (int j = 0; j < y; j++)
+		//		{
+		//			array[i, j] = (++count).ToString();
+		//		}
+		//	}
+		//	Console.Write("Enter the number: ");
+		//	string myNumber = Console.ReadLine();
 
-			// search
-			for (int i = 0; i < x; i++)
-			{
-				for (int j = 0; j < y; j++)
-				{
-					if (array[i,j].Equals(myNumber))
-					{
-						goto Found;
-					}
-				}
-			}
+		//	// search
+		//	for (int i = 0; i < x; i++)
+		//	{
+		//		for (int j = 0; j < y; j++)
+		//		{
+		//			if (array[i,j].Equals(myNumber))
+		//			{
+		//				goto Found;
+		//			}
+		//		}
+		//	}
 
-			Console.WriteLine($"The number {myNumber} was not found.");
-			goto Finish;
+		//	Console.WriteLine($"The number {myNumber} was not found.");
+		//	goto Finish;
 
-		Found:
-			Console.WriteLine($"The number {myNumber} is found.");
-		Finish:
-			Console.WriteLine($"End search.");
+		//Found:
+		//	Console.WriteLine($"The number {myNumber} is found.");
+		//Finish:
+		//	Console.WriteLine($"End search.");
 
-		// Keep the console open in debug mode
-			Console.WriteLine("Press any key to exit.");
-			Console.ReadKey();
-		}
+		//// Keep the console open in debug mode
+		//	Console.WriteLine("Press any key to exit.");
+		//	Console.ReadKey();
+		//}
 	}
 }
