626705522d049416b8551f932dcc12b83c22ed22
pwang86 <pwang86@gmail.com>
2019-10-03 17:20:28 +1000

    add check demo

---
+++ Demo/OverFlowTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+	class OverFlowTest
+	{
+		// all member declare static, which belongs to the type itself,not specific object
+		// const int ConstantMax = int.MaxValue;
+		static int maxIntValue = 2147483647;
+
+		static int CheckedMethod()
+		{
+			int z = 0;
+			try
+			{
+				z = checked(maxIntValue + 10);
+			}
+			catch (System.OverflowException e)
+			{
+				Console.WriteLine("CHECKED AND CAUGHT: " + e.ToString());
+			}
+			return z;
+		}
+
+		static int UncheckedMethod()
+		{
+			int z = 0;
+			try
+			{
+				z = maxIntValue + 10;
+			}
+			catch (System.OverflowException e)
+			{
+				Console.WriteLine("CHECKED AND CAUGHT: " + e.ToString());
+			}
+			return z;
+		}
+
+		static void Main()
+		{
+			Console.WriteLine("\nCHECKED OUTPUT VALUE IS: {0}", CheckedMethod());
+			Console.WriteLine("UNCHECKED OUTPUT VALUE IS: {0}", UncheckedMethod());
+		}
+	}
+}
--- Demo/ThrowDemo.cs
+++ Demo/ThrowDemo.cs
@@ -8,35 +8,35 @@ namespace Demo
 {
 	class ThrowDemo
 	{
-		static void ProcessString(string s)
-		{
-			if (s == null)
-			{
-				throw new ArgumentNullException();
-			}
-		}
+		//static void ProcessString(string s)
+		//{
+		//	if (s == null)
+		//	{
+		//		throw new ArgumentNullException();
+		//	}
+		//}
 
-		static void Main()
-		{
-			try
-			{
-				string s = null;
-				ProcessString(s);
-			}
+		//static void Main()
+		//{
+		//	try
+		//	{
+		//		string s = null;
+		//		ProcessString(s);
+		//	}
 
 			
 
-			catch (ArgumentNullException e)
-			{
-				Console.WriteLine("{0} First exception caught.", e);
-			}
-			// if the below block is placed before the first catch block, error will appear: A previous catch clasue already catches....
-			catch (Exception e)
-			{
-				Console.WriteLine("{0} second exception caught.", e);
-			}
+		//	catch (ArgumentNullException e)
+		//	{
+		//		Console.WriteLine("{0} First exception caught.", e);
+		//	}
+		//	// if the below block is placed before the first catch block, error will appear: A previous catch clasue already catches....
+		//	catch (Exception e)
+		//	{
+		//		Console.WriteLine("{0} second exception caught.", e);
+		//	}
 
-		}
+		//}
 
 
 	}
