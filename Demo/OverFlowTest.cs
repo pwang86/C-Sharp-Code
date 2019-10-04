dcebc570370701d774f524062ebab5abcd556553
pwang86 <pwang86@gmail.com>
2019-10-04 11:38:29 +1000

    Add lock demo

--- Demo/OverFlowTest.cs
+++ Demo/OverFlowTest.cs
@@ -8,42 +8,42 @@ namespace Demo
 {
 	class OverFlowTest
 	{
-		// all member declare static, which belongs to the type itself,not specific object
-		// const int ConstantMax = int.MaxValue;
-		static int maxIntValue = 2147483647;
+	//	// all member declare static, which belongs to the type itself,not specific object
+	//	// const int ConstantMax = int.MaxValue;
+	//	static int maxIntValue = 2147483647;
 
-		static int CheckedMethod()
-		{
-			int z = 0;
-			try
-			{
-				z = checked(maxIntValue + 10);
-			}
-			catch (System.OverflowException e)
-			{
-				Console.WriteLine("CHECKED AND CAUGHT: " + e.ToString());
-			}
-			return z;
-		}
+	//	static int CheckedMethod()
+	//	{
+	//		int z = 0;
+	//		try
+	//		{
+	//			z = checked(maxIntValue + 10);
+	//		}
+	//		catch (System.OverflowException e)
+	//		{
+	//			Console.WriteLine("CHECKED AND CAUGHT: " + e.ToString());
+	//		}
+	//		return z;
+	//	}
 
-		static int UncheckedMethod()
-		{
-			int z = 0;
-			try
-			{
-				z = maxIntValue + 10;
-			}
-			catch (System.OverflowException e)
-			{
-				Console.WriteLine("CHECKED AND CAUGHT: " + e.ToString());
-			}
-			return z;
-		}
+	//	static int UncheckedMethod()
+	//	{
+	//		int z = 0;
+	//		try
+	//		{
+	//			z = maxIntValue + 10;
+	//		}
+	//		catch (System.OverflowException e)
+	//		{
+	//			Console.WriteLine("CHECKED AND CAUGHT: " + e.ToString());
+	//		}
+	//		return z;
+	//	}
 
-		static void Main()
-		{
-			Console.WriteLine("\nCHECKED OUTPUT VALUE IS: {0}", CheckedMethod());
-			Console.WriteLine("UNCHECKED OUTPUT VALUE IS: {0}", UncheckedMethod());
-		}
+	//	static void Main()
+	//	{
+	//		Console.WriteLine("\nCHECKED OUTPUT VALUE IS: {0}", CheckedMethod());
+	//		Console.WriteLine("UNCHECKED OUTPUT VALUE IS: {0}", UncheckedMethod());
+	//	}
 	}
 }
