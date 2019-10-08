f562599ab6aa96e8955803b17c5e7617793177d9
pwang86 <pwang86@gmail.com>
2019-10-08 22:06:57 +1100

    add byte demo

---
+++ Demo/ArithmeticDemo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+	class ArithmeticDemo
+	{
+		static void Main()
+		{
+			//// 1. byte example
+			//byte a = 200;
+			//byte b = 100;
+
+			//var c = a + b;
+			//Console.WriteLine(c.GetType());
+			//Console.WriteLine(c);
+
+			//a += b;
+			//Console.WriteLine(b);
+
+			//Console.WriteLine(a); 
+			//// output: 44, because byte is 0 - 255, after +, a becomes 300 which is greater than 256,
+			//// so it begins counting again: 300 - 256 = 44.  
+			///
+
+
+		}
+	}
+}
