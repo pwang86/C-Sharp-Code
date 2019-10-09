2d2f2c0cc635a2739edcf3c7c3f4f5d5f7ef4fe4
pwang86 <pwang86@gmail.com>
2019-10-09 11:08:04 +1100

    add null and precedence demo

--- Demo/ArithmeticDemo.cs
+++ Demo/ArithmeticDemo.cs
@@ -37,26 +37,51 @@ namespace Demo
 			//Console.WriteLine(b);
 			//Console.WriteLine(double.IsNaN(b));
 
-			//3. Round-off error
-			Console.WriteLine(.41f % .2f);
+			////3. Round-off error
+			//Console.WriteLine(.41f % .2f);
 
-			float x = 0.1f;
-			float y = 3 * x;
-			Console.WriteLine(x == y);
+			//float x = 0.1f;
+			//float y = 3 * x;
+			//Console.WriteLine(x == y);
 
-			double a = 0.1;
-			double b = 3 * a;
-			double c = 0.3;
-			double f = 3 * a;
-			Console.WriteLine(b == c);
-			Console.WriteLine(b.Equals(c));
-			Console.WriteLine(b == f);
-			Console.WriteLine(b);
+			//double a = 0.1;
+			//double b = 3 * a;
+			//double c = 0.3;
+			//double f = 3 * a;
+			//Console.WriteLine(b == c);
+			//Console.WriteLine(b.Equals(c));
+			//Console.WriteLine(b == f);
+			//Console.WriteLine(b);
 
-			decimal d = 1 / 3.0m;
-			decimal e = 3 * d;
-			Console.WriteLine(e == 1.0m);
-			Console.WriteLine(e);
+			//decimal d = 1 / 3.0m;
+			//decimal e = 3 * d;
+			//Console.WriteLine(e == 1.0m);
+			//Console.WriteLine(e);
+
+			//// 4. null
+			//bool? test = null;
+			//Display(!test);
+			//Display(test ^ false);
+			//Display(test ^ null);
+			//Display(true ^ null);
+
+			//void Display(bool? b) => Console.WriteLine(b is null ? "null" : b.Value.ToString());
+
+			// 5. precedence
+			Console.WriteLine(true | true & false);
+			Console.WriteLine((true | true) & false);
+
+			bool Operand(string name, bool value)
+			{
+				Console.WriteLine($"Operand {name} is evaluated.");
+				return true;
+			}
+
+			var byDefaultPrecedence = Operand("A", true) || Operand("B", true) && Operand("C", false);
+			Console.WriteLine(byDefaultPrecedence);
+
+			var changeOrder = (Operand("A", true) || Operand("B", true)) && Operand("C", false);
+			Console.WriteLine(changeOrder);
 		}
 	}
 }
