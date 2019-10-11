642b7cddbec52ee2f19af36d93c16f1dbb7e111f
pwang86 <pwang86@gmail.com>
2019-10-11 17:15:17 +1100

    add ref demo

--- Demo/UserDefinedDemo.cs
+++ Demo/UserDefinedDemo.cs
@@ -8,38 +8,38 @@ namespace Demo
 {
 	class UserDefinedDemo
 	{
-		public readonly struct Digit
-		{
-			private readonly byte digit;
+		//public readonly struct Digit
+		//{
+		//	private readonly byte digit;
 
-			public Digit(byte digit)
-			{
-				if (digit > 9)
-				{
-					throw new ArgumentOutOfRangeException(nameof(digit), "Digit cannot be greater than nine.");
-				}
-				this.digit = digit;
-			}
+		//	public Digit(byte digit)
+		//	{
+		//		if (digit > 9)
+		//		{
+		//			throw new ArgumentOutOfRangeException(nameof(digit), "Digit cannot be greater than nine.");
+		//		}
+		//		this.digit = digit;
+		//	}
 		
 
-			public static implicit operator byte(Digit d) => d.digit;
-			public static explicit operator Digit(byte b) => new Digit(b);
+		//	public static implicit operator byte(Digit d) => d.digit;
+		//	public static explicit operator Digit(byte b) => new Digit(b);
 
-			public override string ToString() => $"digit";
-		}
+		//	public override string ToString() => $"digit";
+		//}
 
-		public static class UserDefinedConversions
-		{
-			public static void Main()
-			{
-				var d = new Digit(7);
+		//public static class UserDefinedConversions
+		//{
+		//	public static void Main()
+		//	{
+		//		var d = new Digit(7);
 
-				byte number = d;
-				Console.WriteLine(number);
+		//		byte number = d;
+		//		Console.WriteLine(number);
 
-				Digit digit = (Digit)number;
-				Console.WriteLine(digit);
-			}
-		}
+		//		Digit digit = (Digit)number;
+		//		Console.WriteLine(digit);
+		//	}
+		//}
 	}
 }
