035cde430d4a3289d758b9d64b6988bf9cb090c5
pwang86 <pwang86@gmail.com>
2019-10-12 17:57:51 +1100

    add delegate removal demo

---
+++ Demo/DelegateRemovalDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+	class DelegateRemovalDemo
+	{
+		//static void Main()
+		//{
+		//	Action a = () => Console.Write("a");
+		//	Action b = () => Console.Write("b");
+
+		//	var abbaab = a + b + b + b + b + b;
+		//	var aba = a + b + a;
+
+		//	var first = abbaab - aba;
+		//	first();
+		//	Console.WriteLine();
+		//	Console.WriteLine(object.ReferenceEquals(abbaab, first));
+
+		//	Action a2 = () => Console.Write("a");
+		//	var changed = aba - a;
+		//	changed();
+		//	Console.WriteLine();
+		//	var unchanged = aba - a2;
+		//	unchanged();
+		//	Console.WriteLine();
+		//	Console.WriteLine(object.ReferenceEquals(aba, unchanged));
+		}
+	}
+}
