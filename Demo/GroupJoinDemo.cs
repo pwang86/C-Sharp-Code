cfbac4aae299a17095b85395a6fd98d49d836223
pwang86 <pwang86@gmail.com>
2019-10-17 16:11:16 +1100

    Add order join demo

--- Demo/GroupJoinDemo.cs
+++ Demo/GroupJoinDemo.cs
@@ -9,47 +9,59 @@ namespace Demo
 {
 	class GroupJoinDemo
 	{
-		static void Main()
-		{
-			Person magnus = new Person { FirstName = "Magnus", LastName = "Hedlund" };
-			Person terry = new Person { FirstName = "Terry", LastName = "Adams" };
-			Person charlotte = new Person { FirstName = "Charlotte", LastName = "Weiss" };
-			Person arlene = new Person { FirstName = "Arlene", LastName = "Huff" };
+		//static void Main()
+		//{
+		//	Person magnus = new Person { FirstName = "Magnus", LastName = "Hedlund" };
+		//	Person terry = new Person { FirstName = "Terry", LastName = "Adams" };
+		//	Person charlotte = new Person { FirstName = "Charlotte", LastName = "Weiss" };
+		//	Person arlene = new Person { FirstName = "Arlene", LastName = "Huff" };
 
-			Pet barley = new Pet { Name = "Barley", Owner = terry };
-			Pet boots = new Pet { Name = "Boots", Owner = terry };
-			Pet whiskers = new Pet { Name = "Whiskers", Owner = charlotte };
-			Pet bluemoon = new Pet { Name = "Blue Moon", Owner = terry };
-			Pet daisy = new Pet { Name = "Daisy", Owner = magnus };
+		//	Pet barley = new Pet { Name = "Barley", Owner = terry };
+		//	Pet boots = new Pet { Name = "Boots", Owner = terry };
+		//	Pet whiskers = new Pet { Name = "Whiskers", Owner = charlotte };
+		//	Pet bluemoon = new Pet { Name = "Blue Moon", Owner = terry };
+		//	Pet daisy = new Pet { Name = "Daisy", Owner = magnus };
 
-			// Create two lists.
-			List<Person> people = new List<Person> { magnus, terry, charlotte, arlene };
-			List<Pet> pets = new List<Pet> { barley, boots, whiskers, bluemoon, daisy };
+		//	// Create two lists.
+		//	List<Person> people = new List<Person> { magnus, terry, charlotte, arlene };
+		//	List<Pet> pets = new List<Pet> { barley, boots, whiskers, bluemoon, daisy };
 
-			var queryTwoSources = from person in people
-								  join pet in pets on person equals pet.Owner into gj
-								  select new { OwnerName = person.FirstName, Pets = gj };
+		//	var queryTwoSources = from person in people
+		//						  join pet in pets on person equals pet.Owner into gj
+		//						  select new { OwnerName = person.FirstName, Pets = gj };
+		//	foreach (var item in queryTwoSources)
+		//	{
+		//		Console.WriteLine($"{item.OwnerName}: ");
+		//		foreach (var inner in item.Pets)
+		//		{
+		//			Console.WriteLine($" {inner.Name}");
+		//		}
+		//	}
 
-			// Create XML
-			XElement ownerAndPets = new XElement("PetOwners",
-				from person in people
-				join pet in pets on person equals pet.Owner into gj
-				select new XElement("Person",
-					new XAttribute("FirstName", person.FirstName),
-					new XAttribute("LastName", person.LastName),
-					from subpet in gj
-					select new XElement("Pet", subpet.Name)));
-			Console.WriteLine(ownerAndPets);
+		//	// Create XML
+		//	XElement ownerAndPets = new XElement("PetOwners",
+		//		from person in people
+		//		join pet in pets on person equals pet.Owner into gj
+		//		select new XElement("Person",
+		//			new XAttribute("FirstName", person.FirstName),
+		//			new XAttribute("LastName", person.LastName),
+		//			from subpet in gj
+		//			select new XElement("Pet", subpet.Name)));
+		//	Console.WriteLine(ownerAndPets);
 
-			foreach (var item in queryTwoSources)
-			{
-				Console.WriteLine($"{item.OwnerName}: ");
-				foreach (var inner in item.Pets)
-				{
-					Console.WriteLine($" {inner.Name}");
-				}
-			}
-		}
+		//	// left outer join
+		//	// Note, if ?. is ignored, exception occurs, because subpet has no match in the gj.
+		//	// so subpet.Name returns null.
+		//	var query = from person in people
+		//				join pet in pets on person equals pet.Owner into gj
+		//				from subpet in gj.DefaultIfEmpty()
+		//				select new { OwnerName = person.FirstName, Pets = subpet?.Name??string.Empty };
+		//	foreach (var v in query)
+		//	{
+		//		Console.WriteLine($"{v.OwnerName + ":",-15}{v.Pets}");
+		//	}
+			
+		// }
 
 		class Person
 		{
---
+++ Demo/OrderJoinDemo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+	class OrderJoinDemo
+	{
+		class Product
+		{
+			public string Name { get; set; }
+			public int CategoryID { get; set; }
+		}
+
+		class Category
+		{
+			public string Name { get; set; }
+			public int ID { get; set; }
+		}
+
+		List<Category> categories = new List<Category>()
+		{
+			new Category(){Name="Beverages", ID=001},
+			new Category(){ Name="Condiments", ID=002},
+			new Category(){ Name="Vegetables", ID=003},
+			new Category() {  Name="Grains", ID=004},
+			new Category() {  Name="Fruit", ID=005}
+		};
+
+		List<Product> products = new List<Product>()
+		{
+			new Product{Name="Cola",  CategoryID=001},
+			new Product{Name="Tea",  CategoryID=001},
+			new Product{Name="Mustard", CategoryID=002},
+			new Product{Name="Pickles", CategoryID=002},
+			new Product{Name="Carrots", CategoryID=003},
+			new Product{Name="Bok Choy", CategoryID=003},
+			new Product{Name="Peaches", CategoryID=005},
+			new Product{Name="Melons", CategoryID=005},
+		};
+
+		//static void Main()
+		//{
+		//	OrderJoinDemo app = new OrderJoinDemo();
+		//	app.OrderJoin();
+
+		//	Console.WriteLine("Press any key");
+		//	Console.ReadKey();
+		//}
+		void OrderJoin()
+		{
+			var query = from c in categories
+						join p in products on c.ID equals p.CategoryID into gj
+						orderby c.Name
+						select new
+						{
+							CategoryName = c.Name,
+							ProductsName = from p1 in gj
+										  orderby p1.Name
+										  select p1
+						};
+
+			foreach (var item in query)
+			{
+				Console.WriteLine($"{item.CategoryName}");
+				foreach (var inner in item.ProductsName)
+				{
+					Console.WriteLine($"   {inner.Name,-10} {inner.CategoryID}");
+				}
+				
+			
+			}
+		}
+	}
+}
