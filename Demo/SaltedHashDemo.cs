bde095daf3f1506fb408f339f9dfe043afae5801
pwang86 <pwang86@gmail.com>
2019-10-27 21:50:38 +1100

    add singleton demo (version 1)

--- Demo/SaltedHashDemo.cs
+++ Demo/SaltedHashDemo.cs
@@ -10,28 +10,28 @@ namespace Demo
 {
 	class SaltedHashDemo
 	{
-		public static void Main(string[] args)
-		{
-			Console.Write("Enter a password: ");
-			string password = Console.ReadLine();
+		//public static void Main(string[] args)
+		//{
+		//	Console.Write("Enter a password: ");
+		//	string password = Console.ReadLine();
 
-			// generate a 128-bit salt 
-			byte[] salt = new byte[16];
-			using (RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider())
-			{
-				rngCsp.GetBytes(salt);
-			}
-			Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+		//	// generate a 128-bit salt 
+		//	byte[] salt = new byte[16];
+		//	using (RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider())
+		//	{
+		//		rngCsp.GetBytes(salt);
+		//	}
+		//	Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
 
-			var pbkdf2 = new Rfc2898DeriveBytes(password,salt,10000);
-			byte[] hash = pbkdf2.GetBytes(20);
-			byte[] hashBytes = new byte[36];
-			Array.Copy(salt, 0, hashBytes, 0, 16);
-			Array.Copy(hash, 0, hashBytes, 16, 20);
+		//	var pbkdf2 = new Rfc2898DeriveBytes(password,salt,10000);
+		//	byte[] hash = pbkdf2.GetBytes(20);
+		//	byte[] hashBytes = new byte[36];
+		//	Array.Copy(salt, 0, hashBytes, 0, 16);
+		//	Array.Copy(hash, 0, hashBytes, 16, 20);
 
-			string savedPasswordHash = Convert.ToBase64String(hashBytes);
+		//	string savedPasswordHash = Convert.ToBase64String(hashBytes);
 		
-			Console.WriteLine($"Hashed: {savedPasswordHash}");
-		}
+		//	Console.WriteLine($"Hashed: {savedPasswordHash}");
+		//}
 	}
 }
---
+++ Demo/SingletonDemo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+	public sealed class SingletonDemo
+	{
+		// Version 1, not thread safe
+		private static SingletonDemo instance = null;
+
+		private SingletonDemo() { }
+
+		public static SingletonDemo Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					instance = new SingletonDemo();
+				}
+				return instance;
+			}
+		}
+	}
+}
