38b973c8b004ffb1f133a77b45afb448f5296f6d
pwang86 <pwang86@gmail.com>
2019-11-18 17:21:09 +1100

    add hashtable demo

---
+++ Demo/HashTableDemo.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+	class HashTableDemo
+	{
+		//public static void Main() {
+
+		//	Hashtable openWith = new Hashtable();
+
+		//	openWith.Add("txt", "notepad.exe");
+		//	openWith.Add("bmp", "paint.exe");
+		//	openWith.Add("dib", "paint.exe");
+		//	openWith.Add("rtf", "wordpad.exe");
+
+		//	try
+		//	{
+		//		openWith.Add("txt", "winword.exe");
+		//	}
+		//	catch
+		//	{
+		//		Console.WriteLine("An element with Key = \"txt\" already exists");
+		//	}
+
+		//	Console.WriteLine("For key = \"rtf\", value = {0}.",openWith["rtf"]);
+
+		//	openWith["rtf"] = "winword.exe";
+		//	Console.WriteLine("For key = \"rtf\",value = {0}.", openWith["rtf"]);
+
+		//	openWith["doc"] = "winword.exe";
+
+		//	if (!openWith.ContainsKey("ht"))
+		//	{
+		//		openWith.Add("ht", "hypertrm.exe");
+		//		Console.WriteLine("Value added for key = \"ht\": {0}", openWith["ht"]);
+
+		//	}
+
+		//	Console.WriteLine();
+		//	foreach (DictionaryEntry de in openWith)
+		//	{
+		//		Console.WriteLine("Key={0},value={1}", de.Key, de.Value);
+
+		//	}
+
+		//	ICollection valueColl = openWith.Values;
+		//	Console.WriteLine();
+
+		//	foreach (string s in valueColl)
+		//	{
+		//		Console.WriteLine("Value = {0}", s);
+		//	}
+
+		//	ICollection keyColl = openWith.Keys;
+
+		//	Console.WriteLine();
+		//	foreach (string s in keyColl)
+		//	{
+		//		Console.WriteLine("Key = {0}", s);
+		//	}
+
+		//	Console.WriteLine("\nRemove(\"doc\")");
+		//	openWith.Remove("doc");
+
+		//	if (!openWith.ContainsKey("doc"))
+		//	{
+		//		Console.WriteLine("key \"doc\" is not found.");
+		//	}
+		//}
+	}
+}
