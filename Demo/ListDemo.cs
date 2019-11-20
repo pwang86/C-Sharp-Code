f6ffad299610153e7d9920904a3e623bb6ea5b8c
pwang86 <pwang86@gmail.com>
2019-11-20 15:33:24 +1100

    Add point comparision

--- Demo/ListDemo.cs
+++ Demo/ListDemo.cs
@@ -8,84 +8,85 @@ namespace Demo
 {
 	class ListDemo
 	{
-		public class Part : IEquatable<Part>
-		{
-			public string PartName { get; set; }
+		//public class Part : IEquatable<Part>
+		//{
+		//	public string PartName { get; set; }
 
-			public int PartId { get; set; }
+		//	public int PartId { get; set; }
 
-			public override string ToString()
-			{
-				return "ID: " + PartId + "   Name: " + PartName;
-			}
-			public override bool Equals(object obj)
-			{
-				if (obj == null) return false;
-				Part objAsPart = obj as Part;
-				if (objAsPart == null) return false;
-				else return Equals(objAsPart);
-			}
-			public override int GetHashCode()
-			{
-				return PartId;
-			}
-			public bool Equals(Part other)
-			{
-				if (other == null) return false;
-				return (this.PartId.Equals(other.PartId));
-			}
-		}
+		//	public override string ToString()
+		//	{
+		//		return "ID: " + PartId + "   Name: " + PartName;
+		//	}
+		//	public override bool Equals(object obj)
+		//	{
+		//		if (obj == null) return false;
+		//		Part objAsPart = obj as Part;
+		//		if (objAsPart == null) return false;
+		//		else return Equals(objAsPart);
+		//	}
+		//	public override int GetHashCode()
+		//	{
+		//		return PartId;
+		//	}
+		//	public bool Equals(Part other)
+		//	{
+		//		if (other == null) return false;
+		//		return (this.PartId.Equals(other.PartId));
+		//	}
+		//}
 
-		public class Example
-		{
-			public static void Main()
-			{
-				List<Part> parts = new List<Part>();
+		//public class Example
+		//{
+		//	public static void Main()
+		//	{
+		//		List<Part> parts = new List<Part>();
 
-				parts.Add(new Part { PartName = "crank arm", PartId = 1234 });
-				parts.Add(new Part { PartName = "chain ring", PartId = 1334 });
-				parts.Add(new Part { PartName = "regular seat", PartId = 1434 });
-				parts.Add(new Part { PartName = "banana seat", PartId = 1444 });
-				parts.Add(new Part { PartName = "cassette", PartId = 1534 });
-				parts.Add(new Part { PartName = "shift lever", PartId = 1634 });
+		//		parts.Add(new Part { PartName = "crank arm", PartId = 1234 });
+		//		parts.Add(new Part { PartName = "chain ring", PartId = 1334 });
+		//		parts.Add(new Part { PartName = "regular seat", PartId = 1434 });
+		//		parts.Add(new Part { PartName = "banana seat", PartId = 1444 });
+		//		parts.Add(new Part { PartName = "cassette", PartId = 1534 });
+		//		parts.Add(new Part { PartName = "shift lever", PartId = 1634 });
 
-				Console.WriteLine();
-				foreach (Part aPart in parts)
-				{
-					Console.WriteLine(aPart);
-				}
+		//		Console.WriteLine();
+		//		foreach (Part aPart in parts)
+		//		{
+		//			Console.WriteLine(aPart);
+		//		}
 
-				Console.WriteLine("\nContains(\"1734\"):{0}",parts.Contains(new Part { PartId = 1734, PartName = "" }));
+		//		Console.WriteLine("\nContains(\"1734\"):{0}",parts.Contains(new Part { PartId = 1734, PartName = "" }));
 
-				Console.WriteLine("\nInsert(2,\"1834\")");
-				parts.Insert(2, new Part { PartName = "brake lever", PartId = 1834 });
+		//		Console.WriteLine("\nInsert(2,\"1834\")");
+		//		parts.Insert(2, new Part { PartName = "brake lever", PartId = 1834 });
 
-				foreach (Part aPart in parts)
-				{
-					Console.WriteLine(aPart);
-				}
+		//		foreach (Part aPart in parts)
+		//		{
+		//			Console.WriteLine(aPart);
+		//		}
 
-				Console.WriteLine("\nParts[3]:{0}", parts[3]);
-				Console.WriteLine("\nRemove(\"1534\")");
+		//		Console.WriteLine("\nParts[3]:{0}", parts[3]);
+		//		Console.WriteLine("\nRemove(\"1534\")");
 
-				parts.Remove(new Part { PartId = 1534, PartName = "cogs" });
+		//		parts.Remove(new Part { PartId = 1534, PartName = "cogs" });
 
-				Console.WriteLine();
+		//		Console.WriteLine();
 
-				foreach (Part aPart in parts)
-				{
-					Console.WriteLine(aPart);
-				}
-				Console.WriteLine("\nRemoveAt(3)");
+		//		foreach (Part aPart in parts)
+		//		{
+		//			Console.WriteLine(aPart);
+		//		}
+		//		Console.WriteLine("\nRemoveAt(3)");
 
-				parts.RemoveAt(3);
+		//		parts.RemoveAt(3);
 
-				Console.WriteLine();
-				foreach (Part aPart in parts)
-				{
-					Console.WriteLine(aPart);
-				}
-			}
-		}
+		//		Console.WriteLine();
+		//		foreach (Part aPart in parts)
+		//		{
+		//			Console.WriteLine(aPart);
+		//		}
+		//	}
+		//}
+		
 	}
 }
---
+++ Demo/PointComparision.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+	class PointComparision
+	{
+		class Point
+		{
+			protected int x, y;
+
+			public Point() : this(0, 0) { }
+
+			public Point(int x, int y)
+			{
+				this.x = x;
+				this.y = y;
+			}
+
+			public override bool Equals(object obj)
+			{
+				if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+				{
+					return false;
+				}
+				else {
+					Point p = (Point)obj;
+					return (x == p.x) && (y == p.y);
+				}
+			}
+
+			public override int GetHashCode()
+			{
+				return (x << 2) ^ y;
+			}
+
+			public override string ToString()
+			{
+				return String.Format("Point({0},{1})", x, y);
+			}
+		}
+
+		sealed class Point3D : Point
+		{
+			int z;
+
+			public Point3D(int x, int y, int z) : base(x, y)
+			{
+				this.z = z;
+			}
+
+			public override bool Equals(object obj)
+			{
+				Point3D pt3 = obj as Point3D;
+				if (pt3 == null)
+					return false;
+				else
+					return base.Equals((Point)obj) && z== pt3.z;
+			}
+
+			public override int GetHashCode()
+			{
+				return (base.GetHashCode() << 2) ^ z;	
+			}
+
+			public override string ToString()
+			{
+				return String.Format("Point({0,{1},{2}})",x,y,z);
+			}
+		}
+
+		class Example
+		{
+			public static void Main()
+			{
+				Point point2D = new Point(5, 5);
+				Point3D point3Da = new Point3D(5, 5, 2);
+				Point3D point3Db = new Point3D(5, 5, 2);
+				Point3D point3Dc = new Point3D(5, 5, -1);
+
+				Console.WriteLine("{0} = {1}: {2}",
+								  point2D, point3Da, point2D.Equals(point3Da));
+				Console.WriteLine("{0} = {1}: {2}",
+								  point2D, point3Db, point2D.Equals(point3Db));
+				Console.WriteLine("{0} = {1}: {2}",
+								  point3Da, point3Db, point3Da.Equals(point3Db));
+				Console.WriteLine("{0} = {1}: {2}",
+								  point3Da, point3Dc, point3Da.Equals(point3Dc));
+			}
+		}
+	
+	}
+}
